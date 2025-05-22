346d45d9364a8ffebc57187e717384f196945c33
Ali <mehdizadehali73@gmail.com>
2025-05-22 17:27:39 +0330

    Add modern color palette and refactor AddForm.css to use CSS variables for consistent theming

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Header from "./components/Header/Header";
 import AddForm from "./components/Booklist/AddForm";
+// import 'App.css'
 
 export default class App extends Component {
   render() {
